7464346a35172aa258cf45ca7e9340a4d10d1e15
Alex Braun <alexander.g.braun@gmail.com>
2020-04-28 12:33:22 -0700

    added validators

--- python/nerve/validators.py
+++ python/nerve/validators.py
@@ -1,205 +1,242 @@
-#! /usr/bin/env python
 '''
-The validators module is function library for validating singular traits given to a specification
+The validators module is function library for validating singular traits given
+to a specification.
 
-Much like the traits module, validator functions follow a "is_"[trait] naming
-convention.  Validators are linked with traits inside the validators kwarg of a
-specification class attribute in the specifications module.  They succeed
-silently and raise ValidationError when the trait they validate fails.
-Schematics captures these error messages and pipes them to a error call.
-
-Functions should generally be wrapped with the is_a or is_re decorators for
-brevity.
+Validators are linked with traits via the validators kwarg of a
+specification class attribute. They succeed silently and raise DataError when
+the trait they validate fails. Schematics captures these error messages and
+pipes them to an error call.
 '''
-# ------------------------------------------------------------------------------
 
-import os
 import re
-from functools import wraps
-from schematics.exceptions import ValidationError
-import nerve
+
+from pyparsing import ParseException
+import wrapt
+
+from nerve.parser import AssetNameParser
 # ------------------------------------------------------------------------------
 
-def is_a(predicate=lambda x: x != None, message=''):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(items):
 
-            if isinstance(items, dict):
-                items = items.items()
-            elif items.__class__.__name__ not in ['list', 'tuple']:
-                items = [items]
+class ValidationError(Exception):
+    '''Error raised for validators.'''
+    pass
 
-            for item in items:
-                if predicate(item):
-                    if func(item) == None:
-                        continue
-                raise ValidationError(str(item) + ' ' + message)
-            return True
 
-        return wrapper
-    return decorator
+def validator(message):
+    '''
+    A decorator for predicate functions that raises a ValidationError
+    if it returns False.
 
-def is_re(regex, message=''):
-    return is_a(lambda x: re.search('^' + regex + '$', x), message)
-# ------------------------------------------------------------------------------
+    Args:
+        message (str): Error message if predicate returns False.
 
-@is_a(os.path.exists, 'does not exist')
-def is_exists(item): return
+    Raises:
+        ValidationError: If predicate returns False.
 
-@is_a(os.path.isabs, 'is not a valid fullpath')
-def is_path(item): return
+    Returns:
+        function: Function that returns a boolean.
+    '''
+    @wrapt.decorator
+    def wrapper(wrapped, instance, args, kwargs):
+        if not wrapped(*args):
+            args = [str(x) for x in args] * 10
+            msg = message.format(*args)
+            raise ValidationError(msg)
+        return
+    return wrapper
 
-@is_a(os.path.isfile, 'is not a valid file path')
-def is_file(item): return
 
-@is_a(os.path.isdir, 'is not a valid directory path')
-def is_directory(item): return
-# ------------------------------------------------------------------------------
+@validator('"{}" is not a valid project name.')
+def is_project(item):
+    '''
+    Validates a project name.
 
-def is_asset_id(item):return True
+    Args:
+        item (str): Project name.
 
-@is_a(lambda x: x[0] in list('xyz'), 'is not a valid coordinate')
-def is_coordinate(item): return
+    Raises:
+        ValidationError: If project name is invalid.
 
-def is_deliverable_name(items):
-    proj  = '[a-z]+\d\d\d'
-    spec  = '[a-z]+\d\d\d'
-    desc  = '[-a-zA-Z0-9]'
-    ver   = 'v\d\d\d'
-    rpass = '([-a-z0-9]+|-)'
-    coord = '(\d\d\d-\d\d\d-\d\d\d|-)'
-    frame = '(\d\d\d\d|-)'
-    ext   = '(\.[a-zA-Z0-9]+|)'
-    regex = '_'.join([proj, spec, desc, ver, rpass, coord, frame, ext])
-    regex = '^' + regex + '$'
-    regex = re.compile(regex)
+    Returns:
+        bool: Validity of project name.
+    '''
+    try:
+        ind = AssetNameParser.PROJECT_INDICATOR
+        AssetNameParser(['project']).parse(ind + item)
+    except ParseException:
+        return False
 
-    if isinstance(items, str):
-        items = [items]
+    if re.search('^[a-z0-9]+$', item) is None:
+        return False
 
-    for item in items:
-        if regex.search(item):
-            continue
-        raise ValidationError(str(item) + ' is not a valid deliverable name')
     return True
 
-@is_re('[-a-z0-9]+', 'is not a valid descriptor')
-def is_descriptor(item): return
 
-def is_not_empty(item):
-    if len(item) == 0:
-        raise ValidationError('list is empty')
+@validator('"{}" is not a valid descriptor.')
+def is_descriptor(item):
+    '''
+    Validates a descriptor.
+
+    Args:
+        item (str): Descriptor.
+
+    Raises:
+        ValidationError: If descriptor is invalid.
+
+    Returns:
+        bool: Validity of descriptor.
+    '''
+    try:
+        ind = AssetNameParser.DESCRIPTOR_INDICATOR
+        AssetNameParser(['descriptor']).parse(ind + item)
+    except ParseException:
+        return False
+
+    if re.search('^[a-z0-9-]+$', item) is None:
+        return False
+
+    # the mast/final/last asset is never actually that
+    # asset should only ever be thought of in terms of latest version
+    if re.search('^(master|final|last)', item):
+        return False
+
+    if len(item) < 1:
+        return False
+
     return True
 
-@is_re('[a-zA-Z0-9]+', 'is not a valid extension')
-def is_extension(item): return
 
-@is_a(lambda x: x <= 9999, 'is not a valid frame')
-def is_frame(item): return
-
-# @is_re('[.*/a-zA-Z0-9_]+', 'is not a valid gitignore pattern')
-# def is_gitignore(item): return
-
-@is_a(lambda x: x, 'does not contain _meta in name')
-def is_meta(item): return
-
-@is_a(lambda x: x in ['yml', 'yaml'], 'is not a valid metadata extension')
-def is_metadata_extension(item): return
-
-def is_metapath(item):
-    meta = nerve.spec.traits.fetch_name_traits(item)
-    nerve.spec.specifications.MetaName(meta).validate()
-    return True
-
-def is_organization(item):
-    return True
-
-def is_private(item):
-    return True
-
-def is_project_id(item):
-    return True
-
-def is_project_name(item):
-    return True
-
-def is_project_root(item):
-    return True
-
-def is_project_url(item):
-    return True
-
-@is_re('[a-z0-9_\-/*\.]+', 'is not a valid exclude pattern')
-def is_exclude_pattern(item): return
-
-@is_re('[a-z0-9_\-/*\.]+', 'is not a valid include pattern')
-def is_include_pattern(item): return
-
-@is_re('$|^'.join([
-        'beauty',
-        'specular',
-        'diffuse',
-        'shadow',
-        'ambient-occlusion',
-        'world-point-position',
-        'uv',
-        'normal',
-        'depth',
-        'z',
-        'matte'
-    ]),
-    'is not a valid render pass'
-)
-def is_render_pass(item): return
-
-def is_request_exclude_patterns(item):
-    return True
-
-def is_request_include_patterns(item):
-    return True
-
-@is_a(
-    lambda x: hasattr(nerve.spec.specifications, x.capitalize()),
-    'is not a valid specification')
-def is_specification(item): return
-
-@is_re('added|copied|deleted|modified|renamed|updated|untracked',
-    'is not a valid state')
-def is_status_state(item): return
-
-@is_re('nondeliverable|deliverable', 'is not a valid asset type')
-def is_status_asset_type(item): return
-
-def is_teams(item):
-    return True
-
-def is_token(item):
-    return True
-
-@is_re('ssh$|^http', 'is not ssh or http')
-def is_url_type(item): return
-
-def is_user_branch(item):
-    return True
-
-def is_username(item):
-    return True
-
+@validator('{} is not a valid version. 0 < version < 1000.')
 def is_version(item):
+    '''
+    Validates a version.
+
+    Args:
+        item (str): Version.
+
+    Raises:
+        ValidationError: If version is invalid.
+
+    Returns:
+        bool: Validity of version.
+    '''
+    return item > 0 and item < 10**AssetNameParser.VERSION_PADDING
+
+
+@validator('{} is not a valid frame. -1 < frame < 10000.')
+def is_frame(item):
+    '''
+    Validates a frame.
+
+    Args:
+        item (str): Frame.
+
+    Raises:
+        ValidationError: If frame is invalid.
+
+    Returns:
+        bool: Validity of frame.
+    '''
+    return item >= 0 and item < 10**AssetNameParser.FRAME_PADDING
+
+
+@validator('{} is not a valid coordinate. -1 < coordinate < 1000.')
+def is_coordinate(item):
+    '''
+    Validates a coordinate.
+
+    Args:
+        item (str): Coordinate.
+
+    Raises:
+        ValidationError: If coordinate is invalid.
+
+    Returns:
+        bool: Validity of coordinate.
+    '''
+    if len(item) == 0:
+        return False
+
+    if len(item) > 3:
+        return False
+
+    if min(item) < 0:
+        return False
+
+    if max(item) >= 10**AssetNameParser.COORDINATE_PADDING:
+        return False
+
     return True
-# ------------------------------------------------------------------------------
 
-def main():
+
+@validator('"{}" is not a valid extension.')
+def is_extension(item):
     '''
-    Run help if called directly
+    Validates a file extension.
+
+    Args:
+        item (str): File extension.
+
+    Raises:
+        ValidationError: If extension is invalid.
+
+    Returns:
+        bool: Validity of extension.
     '''
+    if re.search('^[a-z0-9]+$', item):
+        return True
+    return False
 
-    import __main__
-    help(__main__)
-# ------------------------------------------------------------------------------
 
-# __all__ = []
+@validator('{} != {}.')
+def is_eq(a, b):
+    '''
+    Validates that a and b are equal.
 
-if __name__ == '__main__':
-    main()
+    Args:
+        a (object): Object.
+        b (object): Object.
+
+    Raises:
+        ValidationError: If a does not equal b.
+
+    Returns:
+        bool: Equality of a and b.
+    '''
+    return a == b
+
+
+@validator('{} !< {}.')
+def is_lt(a, b):
+    '''
+    Validates that a is less than b.
+
+    Args:
+        a (object): Object.
+        b (object): Object.
+
+    Raises:
+        ValidationError: If a is not less than b.
+
+    Returns:
+        bool: A is less than b.
+    '''
+    return a < b
+
+
+@validator('{} !> {}.')
+def is_gt(a, b):
+    '''
+    Validates that a is greater than b.
+
+    Args:
+        a (object): Object.
+        b (object): Object.
+
+    Raises:
+        ValidationError: If a is not greater than b.
+
+    Returns:
+        bool: A is greater than b.
+    '''
+    return a > b
---
+++ python/nerve/validators_test.py
@@ -0,0 +1,126 @@
+import unittest
+
+from schematics.exceptions import ValidationError
+
+import nerve.validators as vd
+from nerve.validators import ValidationError
+# ------------------------------------------------------------------------------
+
+
+class DatabaseTests(unittest.TestCase):
+    def test_validator(self):
+        message = '{} is not foo'
+        @vd.validator(message)
+        def func(item):
+            return item == 'foo'
+
+        func('foo')
+
+        expected = message.format('bar')
+        with self.assertRaisesRegexp(ValidationError, expected):
+            func('bar')
+
+        expected = message.format(1)
+        with self.assertRaisesRegexp(ValidationError, expected):
+            func(1)
+
+    def test_is_project(self):
+        vd.is_project('proj001')
+
+        expected = '"proj-001" is not a valid project name.'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            vd.is_project('proj-001')
+
+        expected = '"bigfancyproj001" is not a valid project name.'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            vd.is_project('bigfancyproj001')
+
+        expected = '"proj00001" is not a valid project name.'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            vd.is_project('proj00001')
+
+    def test_is_descriptor(self):
+        vd.is_descriptor('desc')
+
+        expected = '"master-desc" is not a valid descriptor.'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            vd.is_descriptor('master-desc')
+
+        expected = '"Desc" is not a valid descriptor.'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            vd.is_descriptor('Desc')
+
+        expected = '"" is not a valid descriptor.'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            vd.is_descriptor('')
+
+    def test_is_version(self):
+        vd.is_version(1)
+
+        expected = '-1 is not a valid version.'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            vd.is_version(-1)
+
+        expected = '0 is not a valid version.'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            vd.is_version(0)
+
+        expected = '1000 is not a valid version.'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            vd.is_version(1000)
+
+    def test_is_frame(self):
+        vd.is_frame(59)
+
+        expected = '-1 is not a valid frame.'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            vd.is_frame(-1)
+
+        expected = '10000 is not a valid frame.'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            vd.is_frame(10000)
+
+    def test_is_coordinate(self):
+        vd.is_coordinate([59])
+        vd.is_coordinate([59, 0])
+        vd.is_coordinate([59, 4, 23])
+
+        expected = r'\[-1\] is not a valid coordinate\.'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            vd.is_coordinate([-1])
+
+        expected = r'\[0, 2, -1\] is not a valid coordinate\.'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            vd.is_coordinate([0, 2, -1])
+
+        expected = r'\[0, 0, 1000\] is not a valid coordinate\.'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            vd.is_coordinate([0, 0, 1000])
+
+    def test_is_extension(self):
+        vd.is_extension('png')
+
+        expected = r'"\$\$\$" is not a valid extension\.'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            vd.is_extension('$$$')
+
+    def test_is_eq(self):
+        vd.is_eq(1, 1)
+
+        expected = 'foo != bar.'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            vd.is_eq('foo', 'bar')
+
+    def test_is_lt(self):
+        vd.is_lt(0, 1)
+
+        expected = '1 !< 0.'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            vd.is_lt(1, 0)
+
+    def test_is_gt(self):
+        vd.is_gt(1, 0)
+
+        expected = '0 !> 1.'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            vd.is_gt(0, 1)
