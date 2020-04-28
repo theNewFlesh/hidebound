dc2f4278dec364bd26ab0426ec206f31c1f39f73
Alex Braun <alexander.g.braun@gmail.com>
2020-04-28 14:04:50 -0700

    added specification_base

--- python/nerve/specification_base.py
+++ python/nerve/specification_base.py
@@ -1,250 +1,192 @@
-#! /usr/bin/env python
-'''
-The base module houses all the base specifications for nerve entities
+from pathlib import Path
 
-Those entities include: configs, projects, deliverables and deliverables.
+from nerve.validators import ValidationError
+from pyparsing import ParseException
+from schematics import Model
+from schematics.types import StringType, IntType
 
-All specifications used in production should be subclassed from the
-aforementioned classes.  All class attributes must have a "get_[attribute]"
-function in the traits module and should have one or more validators related t
-the value of that trait (especially if required).
-'''
+from nerve.parser import AssetNameParser
+import nerve.validators as vd
 # ------------------------------------------------------------------------------
 
-import re
-# import nerve
-# from nerve.utils import conform_keys
-from nerve.validators import *
-from schematics.models import Model, BaseType
-from schematics.types import StringType, IntType, BooleanType, URLType
-from schematics.types.compound import ListType, DictType, ModelType
-from schematics.exceptions import ValidationError
-# ------------------------------------------------------------------------------
 
-class Specification(Model):
-    '''
-        Base class from which all nerve specifications are subclassed.
-    '''
-    specification = StringType(required=True, validators=[is_specification])
+'''
+Contains the abstract base classes for all Nerve specifications.
+'''
 
-    def __init__(self, data):
+
+class SpecificationBase(Model):
+    '''
+    The base class for all Nerve specifications.
+
+    Attributes:
+        asset_type (str): Type of asset. Options include: file, sequence, complex.
+        filename_fields (list[str]): List of fields found in the asset filenames.
+        asset_name_fields (list[str]): List of fields found in the asset name.
+        project (str): Project name.
+        descriptor (str): Asset descriptor.
+        version (int): Asset version.
+        extension (str): File extension.
+    '''
+    asset_type = 'specification'
+    filename_fields = [
+        'project', 'specification', 'descriptor', 'version', 'extension'
+    ]
+    asset_name_fields = ['project', 'specification', 'descriptor', 'version']
+    project = StringType(required=True, validators=[vd.is_project])
+    descriptor = StringType(required=True, validators=[vd.is_descriptor])
+    version = IntType(required=True, validators=[vd.is_version])
+    extension = StringType(required=True, validators=[vd.is_extension])
+
+    def __init__(self, data={}):
         '''
-        Creates a Specification instance.
-
-        Attributes:
-            name (str): Same as class name.
+        Returns a new specification instance.
 
         Args:
-            data (dict): Dictionary of specification values.
+            data (dict, optional): Dictionary of asset data.
+        '''
+        super().__init__(raw_data=data)
+        self.name = self.__class__.__name__.lower()
+
+    def get_asset_name(self, filepath):
+        '''
+        Returns the expected asset name give a filepath.
+
+        Args:
+            filepath (str or Path): Fullpath to asset file.
 
         Returns:
-            name
+            str: Asset name.
         '''
-        if 'name' not in data.keys():
-            data['name'] = self.__class__.__name__.lower()
-        super().__init__(raw_data=data)
+        filepath = Path(filepath)
+        data = AssetNameParser(self.filename_fields).parse(filepath.name)
+        return AssetNameParser(self.asset_name_fields).to_string(data)
+
+    def get_asset_path(self, filepath):
+        '''
+        Returns the expected asset path given a filepath.
+
+        Args:
+            filepath (str or Path): Fullpath to asset file.
+
+        Raises:
+            NotImplementedError: If method not defined in subclass.
+
+        Returns:
+            Path: Asset path.
+        '''
+        msg = 'Method must be implemented in subclasses of SpecificationBase.'
+        raise NotImplementedError(msg)
+
+    def get_asset_id(self, filepath):
+        '''
+        Returns a hash of the asset directory or file, depending of asset type.
+
+        Args:
+            filepath (str or Path): Fullpath to asset file.
+
+        Returns:
+            str: Asset id.
+        '''
+        return hash(self.get_asset_path(filepath).as_posix())
+
+    def validate_filepath(self, filepath):
+        '''
+        Attempts to parse the given filepath.
+
+        Args:
+            filepath (str or Path): Fullpath to asset file.
+
+        Raises:
+            ValidationError: If parse fails.
+            ValidationError: If asset directory name is invalid.
+        '''
+        filepath = Path(filepath)
+        try:
+            data = AssetNameParser(self.filename_fields).parse(filepath.name)
+        except ParseException as e:
+            raise ValidationError(repr(e))
+
+        if self.asset_type == 'file':
+            return
+
+        parser = AssetNameParser(self.asset_name_fields)
+        actual = self.get_asset_path(filepath).name
+        try:
+            parser.parse(actual)
+        except ParseException as e:
+            raise ValidationError(repr(e))
+
+        expected = parser.to_string(data)
+        if actual != expected:
+            msg = 'Invalid asset directory name. '
+            msg += f'Expecting: {expected}. Found: {actual} in '
+            msg += f'{filepath.as_posix()}.'
+            raise ValidationError(msg)
+
+    def get_filename_metadata(self, filepath):
+        '''
+        Returns a dictionary of filename metadata from given filepath.
+
+        Args:
+            filepath (str or Path): Fullpath to asset file.
+
+        Returns:
+            dict: Metadata.
+        '''
+        return AssetNameParser(self.filename_fields).parse(Path(filepath).name)
 
 
-# class MetaName(Specification):
-#     '''
-#     Used for validating a metadata file's name
+class FileSpecificationBase(SpecificationBase):
+    '''
+    The base class for asset that consist of a single file.
 
-#     A convenience class used by is_metapath in the validators module
-#     '''
-#     project_name  = StringType(required=True, validators=[is_project_name])
-#     specification = StringType(required=True, validators=[is_specification])
-#     descriptor    = StringType(required=True, validators=[])
-#     version       = IntType(required=True, validators=[is_version])
-#     render_pass   = StringType(validators=[is_render_pass])
-#     coordinate    = DictType(BaseType, validators=[is_coordinate])
-#     frame         = ListType(BaseType, validators=[is_frame])
-#     meta          = BooleanType(required=True, validators=[is_meta])
-#     extension     = StringType(required=True, default='yml', validators=[is_metadata_extension])
-# # ------------------------------------------------------------------------------
+    Attributes:
+        asset_type (str): File.
+    '''
+    asset_type = 'file'
 
-# class ProjectTemplate(Specification):
-#     '''
-#     Project templates are used as overidable defaults for project creation
+    def get_asset_path(self, filepath):
+        '''
+        Returns the filepath.
 
-#     Attributes:
-#         specification (str, optional): same as class name
-#         version (int, optional): project version
-#         teams (dict, optional): Github teams; name, permission pairs
-#         gitignore (list, optional): gitignore patterns
-#         lfs_extensions (list, optional): lfs tracked file extensions
-#         nondeliverables (list, optional): nondeliverable asset specs/patterns
-#         deliverables (list, optional): deliverable asset specs/patterns
-#         private (bool, optional): privacy of Github repo
-#     '''
-#     version         = IntType(validators=[is_version])
-#     teams           = DictType(StringType, validators=[is_teams])
-#     gitignore       = ListType(StringType, validators=[])
-#     lfs_extensions  = ListType(StringType, validators=[is_extension, is_not_empty])
-#     nondeliverables = ListType(StringType, validators=[])
-#     deliverables    = ListType(StringType, validators=[is_specification])
-#     private         = BooleanType(validators=[is_private])
+        Args:
+            filepath (str or Path): Fullpath to asset file.
 
-# class Project(Specification):
-#     '''
-#     Base class for all nerve projects
+        Returns:
+            Path: Asset path.
+        '''
+        return Path(filepath)
 
-#     Attributes:
-#         specification (str): same as class name
-#         project_name (str): name of project
-#         project_id (str): Github repo id
-#         project_url (str): project_url of project's Github repo
-#         notes (str): project notes
-#         private (bool): privacy of Github repo
-#         version (int): project version
-#         teams (dict): Github teams; name, permission pairs
-#         gitignore (list): gitignore patterns
-#         lfs_extensions (list): lfs tracked file extensions
-#         nondeliverables (list, optional): nondeliverable asset specs/patterns
-#         deliverables (list): deliverable asset specs/patterns
-#     '''
-#     project_name    = StringType(required=True, validators=[is_project_name])
-#     project_id      = StringType(required=True, validators=[is_project_id])
-#     project_url     = StringType(required=True, validators=[is_project_url])
-#     notes           = StringType(default='')
 
-#     private         = BooleanType(required=True, validators=[is_private])
-#     version         = IntType(required=True, validators=[is_version])
-#     teams           = DictType(StringType, required=True, validators=[is_teams])
-#     gitignore       = ListType(StringType, required=True, validators=[])
-#     lfs_extensions  = ListType(StringType, required=True, validators=[is_extension, is_not_empty])
-#     nondeliverables = ListType(StringType, default=[], validators=[])
-#     deliverables    = ListType(StringType, required=True, validators=[is_specification])
+class SequenceSpecificationBase(SpecificationBase):
+    '''
+    The base class for assets that consist of a sequence of files under a single
+    directory.
 
-# class Asset(Specification):
-#     '''
-#     Base class for Deliverable and NonDeliverable
+    Attributes:
+        asset_type (str): Sequence.
+    '''
+    asset_type = 'sequence'
 
-#     Attributes:
-#         specification (str): same as class name
-#         project_name (str): name of project
-#         project_id (str): Github repo id
-#         project_url (str): project_url of project's Github repo
-#         notes (str): project notes
-#         asset_name (str): name of asset
-#         asset_id (str): randomly generated uuid
-#         data (list): list of filepaths
-#     '''
-#     project_name = StringType(required=True, validators=[is_project_name])
-#     project_id   = StringType(required=True, validators=[is_project_id])
-#     project_url  = StringType(required=True, validators=[is_project_url])
-#     notes        = StringType(default='')
+    def get_asset_path(self, filepath):
+        '''
+        Returns the directory containing the asset files.
 
-#     asset_name   = StringType(required=True, validators=[])
-#     asset_id     = StringType(required=True, validators=[is_asset_id])
-#     data         = ListType(StringType, validators=[is_file, is_path, is_exists])
+        Args:
+            filepath (str or Path): Fullpath to asset file.
 
-# class NonDeliverable(Asset):
-#     '''
-#     Base class for all nerve non-deliverable assets
+        Returns:
+            Path: Asset path.
+        '''
+        return Path(filepath).parents[0]
 
-#     Attributes:
-#         specification (str): same as class name
-#         project_name (str): name of project
-#         project_id (str): Github repo id
-#         project_url (str): project_url of project's Github repo
-#         notes (str): project notes
-#         asset_name (str): name of asset
-#         asset_id (str): randomly generated uuid
-#         data (list): list of filepaths
-#         asset_type (str): 'nondeliverable'
-#     '''
-#     asset_type   = StringType(default='nondeliverable')
 
-# class Deliverable(Asset):
-#     '''
-#     Base class for all nerve deliverable assets
+class ComplexSpecificationBase(SpecificationBase):
+    '''
+    The base class for assets that consist of multiple directories of files.
 
-#     Attributes:
-#         specification (str): same as class name
-#         project_name (str): name of project
-#         project_id (str): Github repo id
-#         project_url (str): project_url of project's Github repo
-#         notes (str): project notes
-#         asset_name (str): name of asset
-#         asset_id (str): randomly generated uuid
-#         data (list): list of filepaths
-#         descriptor (str): descriptor found in asset name
-#         version (int): version found in asset name
-#         dependencies (list): deliverables asset depends upon. Default: []
-#         asset_type (str): 'deliverable'
-#     '''
-#     descriptor   = StringType(required=True, validators=[is_descriptor])
-#     version      = IntType(required=True, validators=[is_version])
-#     dependencies = ListType(StringType, default=[])
-#     asset_type   = StringType(default='deliverable')
-
-# class Config(Specification):
-#     '''
-#     Base class for all nerve configs (nerverc)
-
-#     Attributes:
-#         specification (str): same as class name
-#         username (str): Github username
-#         user_branch (str): User's branch name
-#         organization (str): Github organization
-#         project_root (str): fullpath to root projects directory
-#         token (str): Github oauth token
-#         private (bool): privacy of Github repo
-#         url_type (str): type of access to Github. currently only ssh. Options: http, ssh
-#         request_include_patterns (list, optional): regular expressions used to include assets within a request
-#             Default: []
-#         request_exclude_patterns (list, optional): regular expressions used to exclude assets within a request
-#             Default: []
-#         publish_include_patterns (list, optional): regular expressions used to include assets within a publish
-#             Default: []
-#         publish_exclude_patterns (list, optional): regular expressions used to exclude assets within a publish
-#             Default: []
-#         status_include_patterns (list, optional): regular expressions used to include assets within a status call
-#             Default: []
-#         status_exclude_patterns (list, optional): regular expressions used to exclude assets within a status call
-#             Default: []
-#         status_states (list, optional): list of allowed states for status call
-#             Default: []. Options: added, copied, deleted, modified, renamed, updated, untracked
-#         status_asset_types (list, optional): list of allowed asset types for status call
-#             Default: []. Options: deliverable, nondeliverable
-#         log_level (str, optional): level of logging. Default: warn
-#         environment (dict): Environment variables
-#         lfs_server_url (str, optional): url of lfs server. Default: http://localhost:8080
-#         git_credentials (list): list of git credentials
-#         timeout (int, optional): maximum timeout in seconds. Default: 100
-#         project_template (dict, optional): project specification
-#     '''
-#     username                 = StringType(required=True, validators=[is_username])
-#     user_branch              = StringType(required=True, validators=[is_user_branch])
-#     organization             = StringType(required=True, validators=[is_organization])
-#     project_root             = StringType(required=True, validators=[is_project_root])
-#     token                    = StringType(required=True, validators=[is_token])
-#     url_type                 = StringType(required=True, validators=[is_url_type])
-#     request_include_patterns = ListType(StringType, default=[], validators=[is_include_pattern])
-#     request_exclude_patterns = ListType(StringType, default=[], validators=[is_exclude_pattern])
-#     publish_include_patterns = ListType(StringType, default=[], validators=[is_include_pattern])
-#     publish_exclude_patterns = ListType(StringType, default=[], validators=[is_exclude_pattern])
-#     status_include_patterns  = ListType(StringType, default=[], validators=[is_include_pattern])
-#     status_exclude_patterns  = ListType(StringType, default=[], validators=[is_exclude_pattern])
-#     status_states            = ListType(StringType, default=[], validators=[is_status_state])
-#     status_asset_types       = ListType(StringType, default=[], validators=[is_status_asset_type])
-#     log_level                = StringType(default='warn')
-#     environment              = DictType(StringType, required=True, validators=[])
-#     lfs_server_url           = URLType(default='http://localhost:8080', required=True)
-#     git_credentials          = ListType(StringType, required=True)
-#     timeout                  = IntType(default=100, required=True)
-#     project_template         = StringType(validators=[is_exists])
-#     private                  = BooleanType(required=True, validators=[is_private])
-
-#     def validate_project(self, key, data):
-#         '''
-#         Validates project metadata
-
-#         Args:
-#             key (None): does nothing
-#             data (dict): project metadata
-
-#         Returns:
-#             None
-#         '''
-#         is_specification(data['specification'])
+    Attributes:
+        asset_type (str): Complex.
+    '''
+    asset_type = 'complex'
---
+++ python/nerve/specification_base_test.py
@@ -0,0 +1,103 @@
+from pathlib import Path
+import unittest
+
+import nerve.specification_base as sb
+from nerve.validators import ValidationError
+# ------------------------------------------------------------------------------
+
+
+class SpecificationBaseTests(unittest.TestCase):
+    filepath = '/tmp/proj001/p-proj001_s-spec001_d-desc_v001/p-proj001_s-spec001_d-desc_v001.ext'
+
+    class Foo(sb.SpecificationBase):
+        def get_asset_path(self, filepath):
+            return Path(filepath).parent
+
+    def test_init(self):
+        result = sb.SpecificationBase().name
+        self.assertEqual(result, 'specificationbase')
+        result = self.Foo().name
+        self.assertEqual(result, 'foo')
+
+    def test_get_asset_name(self):
+        result = self.Foo().get_asset_name(self.filepath)
+        expected = 'p-proj001_s-spec001_d-desc_v001'
+        self.assertEqual(result, expected)
+
+    def test_get_asset_path(self):
+        class Bar(sb.SpecificationBase):
+            pass
+        expected = 'Method must be implemented in subclasses of SpecificationBase.'
+        with self.assertRaisesRegexp(NotImplementedError, expected):
+            Bar().get_asset_path(self.filepath)
+
+        result = self.Foo().get_asset_path(self.filepath)
+        expected = Path('/tmp/proj001/p-proj001_s-spec001_d-desc_v001')
+        self.assertEqual(result, expected)
+
+    def test_get_asset_id(self):
+        result = self.Foo().get_asset_id(self.filepath)
+        expected = hash('/tmp/proj001/p-proj001_s-spec001_d-desc_v001')
+        self.assertEqual(result, expected)
+
+    def test_validate_filepath(self):
+        self.Foo().validate_filepath(self.filepath)
+
+        root = '/tmp/proj001'
+        parent = 'p-proj001_s-spec001_d-desc_v001'
+        filename = 'p-proj001_s-spec001_d-desc_v001.ext'
+
+        bad = 'p-proj001_spec001_d-desc_v001.ext'
+        bad_filename_indicator = Path(root, parent, bad)
+        expected = 'Illegal specification field indicator'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            self.Foo().validate_filepath(bad_filename_indicator)
+
+        bad = 'p-proj001_s-spec001_d-PIZZA_v001.ext'
+        bad_parent = 'p-proj001_s-spec001_d-PIZZA_v001'
+        bad_descriptor_token = Path(root, bad_parent, bad)
+        expected = 'Illegal descriptor field token'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            self.Foo().validate_filepath(bad_descriptor_token)
+
+        bad_parent = 'p-proj001_s-spec001_d-pizza_v001'
+        bad_descriptor_token = Path(root, bad_parent, filename)
+        expected = 'Invalid asset directory name'
+        with self.assertRaisesRegexp(ValidationError, expected):
+            self.Foo().validate_filepath(bad_descriptor_token)
+
+    def test_get_filename_metadata(self):
+        result = self.Foo().get_filename_metadata(self.filepath)
+        expected = dict(
+            project='proj001',
+            specification='spec001',
+            descriptor='desc',
+            version=1,
+            extension='ext',
+        )
+        self.assertEqual(result, expected)
+
+
+class OtherSpecificationBaseTests(unittest.TestCase):
+    filepath = '/tmp/proj001/p-proj001_s-spec001_d-desc_v001/p-proj001_s-spec001_d-desc_v001.ext'
+
+    def test_file_specification_base(self):
+        result = sb.FileSpecificationBase()
+        self.assertEqual(result.asset_type, 'file')
+        result = result.get_asset_path(self.filepath)
+        expected = Path(
+            '/tmp/proj001/p-proj001_s-spec001_d-desc_v001',
+            'p-proj001_s-spec001_d-desc_v001.ext'
+        )
+        self.assertEqual(result, expected)
+
+    def test_sequence_specification_base(self):
+        result = sb.SequenceSpecificationBase()
+        self.assertEqual(result.asset_type, 'sequence')
+        result = result.get_asset_path(self.filepath)
+        expected = Path('/tmp/proj001/p-proj001_s-spec001_d-desc_v001')
+        self.assertEqual(result, expected)
+
+    def test_complex_specification_base(self):
+        result = sb.ComplexSpecificationBase()
+        self.assertEqual(result.asset_type, 'complex')
