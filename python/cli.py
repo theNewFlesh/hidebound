5f453cdff6827196ae055da4e5062798c821ae2f
Alex Braun <alexander.g.braun@gmail.com>
2020-06-04 22:24:39 -0700

    addd mock removal to publish

--- python/cli.py
+++ python/cli.py
@@ -390,10 +390,11 @@ def get_publish_command(info):
     cmd += 'cp /root/{repo}/docker/dev_requirements.txt /tmp/{repo}/; '
     cmd += 'cp /root/{repo}/docker/prod_requirements.txt /tmp/{repo}/; '
     cmd += 'cp -r /root/{repo}/templates /tmp/{repo}/{repo}; '
-    cmd += r"find /tmp/{repo} | grep -E '.*test.*\.py$|__pycache__' "
+    cmd += r"find /tmp/{repo} | grep -E '.*test.*\.py$|mock.*\.py$|__pycache__' "
     cmd += r"| parallel 'rm -rf {x}'; "
     cmd += "find /tmp/{repo} -type f | grep __init__.py | parallel '"
-    cmd += "mv {x} /tmp/delete_me; cat /tmp/delete_me | grep -v test > {x}; "
+    cmd += "mv {x} /tmp/delete_me; cat /tmp/delete_me | grep -v test "
+    cmd += "| grep -v mock > {x}; "
     cmd += "rm /tmp/delete_me'"
     cmd += '"; '
     cmd += '{exec2} python3.7 setup.py sdist; '
