abb7fd7ceb31a33b77b22d513e7195f86b769f8e
Alex Braun <alexander.g.braun@gmail.com>
2020-06-05 17:58:35 -0700

    fixed app/api import bug

--- python/hidebound/server/__init__.py
+++ python/hidebound/server/__init__.py
@@ -1,4 +1,5 @@
-import hidebound.server.api  # noqa F401
+# app needs to come before api
 import hidebound.server.app  # noqa F401
+import hidebound.server.api  # noqa F401
 import hidebound.server.components  # noqa F401
 import hidebound.server.server_tools  # noqa F401
