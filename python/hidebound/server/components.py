d33dfb16ee24e7cfdd50a1cc3fbe9c9b6234356a
Alex Braun <alexander.g.braun@gmail.com>
2021-03-03 15:34:21 -0500

    fixed stylesheet href bug

--- python/hidebound/server/components.py
+++ python/hidebound/server/components.py
@@ -138,7 +138,7 @@ def get_dash_app(server, storage_type='memory'):
     app = dash.Dash(
         __name__,
         server=server,
-        external_stylesheets=['http://0.0.0.0:5000/static/style.css'],
+        external_stylesheets=['/static/style.css'],
     )
     app.layout = html.Div(id='layout', children=[store, tabs, content])
     app.config['suppress_callback_exceptions'] = True
