e2c5d6fda5787750e3cd6f8940efcc1da899a6c3
Alex Braun <alexander.g.braun@gmail.com>
2021-05-17 23:34:14 +0000

    added s3 exporter

---
+++ python/hidebound/exporters/s3_exporter.py
@@ -0,0 +1,109 @@
+from typing import Dict
+
+from io import BytesIO
+import json
+
+from schematics import Model
+from schematics.types import StringType
+import boto3 as boto
+
+from hidebound.exporters.exporter_base import ExporterBase
+import hidebound.core.validators as vd
+# ------------------------------------------------------------------------------
+
+
+class S3Config(Model):
+    '''
+    A class for validating configurations supplied to S3Exporter.
+
+    Attributes:
+        access_key (str): AWS access key.
+        secret_key (str): AWS secret key.
+        bucket (str): AWS bucket name.
+    '''
+    access_key = StringType(required=True)  # type: StringType
+    secret_key = StringType(required=True)  # type: StringType
+    bucket = StringType(required=True, validators=[vd.is_bucket_name])  # type: StringType
+
+
+class S3Exporter(ExporterBase):
+    @staticmethod
+    def from_config(config):
+        # type: (Dict) -> S3Exporter
+        '''
+        Construct a S3Exporter from a given config.
+
+        Args:
+            config (dict): Config dictionary.
+
+        Raises:
+            DataError: If config is invalid.
+
+        Returns:
+            S3Exporter: S3Exporter instance.
+        '''
+        return S3Exporter(**config)
+
+    def __init__(
+        self,
+        access_key,
+        secret_key,
+        bucket,
+    ):
+        # type: (str, str, str) -> None
+        '''
+        Constructs a S3Exporter instances and creates a bucket with given name
+        if it does not exist.
+
+        Args:
+            access_key (str): AWS access key.
+            secret_key (str): AWS secret key.
+            bucket (str): AWS bucket name.
+
+        Raises:
+            DataError: If config is invalid.
+        '''
+        config = dict(
+            access_key=access_key,
+            secret_key=secret_key,
+            bucket=bucket,
+        )
+        S3Config(config).validate()
+        # ----------------------------------------------------------------------
+
+        session = boto.session.Session(
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+        )
+        self._bucket = session.resource('s3').Bucket(bucket)
+        self._bucket.create()
+
+    def _export_asset(self, metadata):
+        # type: (Dict) -> None
+        '''
+        Exports metadata from single JSON file in hidebound/metadata/asset.
+
+        Args:
+            metadata (dict): Asset metadata.
+        '''
+        self._bucket.upload_fileobj(
+            BytesIO(json.dumps(metadata, indent=4).encode('utf-8')),
+            'hidebound/metadata/asset/' + metadata['asset_id'] + '.json',
+        )
+
+    def _export_file(self, metadata):
+        # type: (Dict) -> None
+        '''
+        Exports metadata from single JSON file in hidebound/metadata/file.
+
+        Args:
+            metadata (dict): File metadata.
+        '''
+        self._bucket.upload_fileobj(
+            BytesIO(json.dumps(metadata, indent=4).encode('utf-8')),
+            'hidebound/metadata/file/' + metadata['file_id'] + '.json',
+        )
+        self._bucket.upload_file(
+            metadata['filepath'],
+            'hidebound/content/' + metadata['filepath_relative'],
+        )
---
+++ python/hidebound/exporters/s3_exporter_test.py
@@ -0,0 +1,113 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+import json
+import os
+import unittest
+
+from moto import mock_s3
+from schematics.exceptions import DataError
+import boto3 as boto
+
+from hidebound.exporters.s3_exporter import S3Config, S3Exporter
+# ------------------------------------------------------------------------------
+
+
+class S3ConfigTests(unittest.TestCase):
+    def setUp(self):
+        self.config = dict(
+            access_key='foo',
+            secret_key='bar',
+            bucket='bucket',
+        )
+
+    def test_validate(self):
+        S3Config(self.config).validate()
+
+    def test_bucket(self):
+        self.config['bucket'] = 'BadBucket'
+        with self.assertRaises(DataError):
+            S3Config(self.config).validate()
+# ------------------------------------------------------------------------------
+
+
+class S3ExporterTests(unittest.TestCase):
+    @mock_s3
+    def setUp(self):
+        self.config = dict(
+            access_key='foo',
+            secret_key='bar',
+            bucket='bucket',
+        )
+        self.s3 = boto.session.Session(
+            aws_access_key_id=self.config['access_key'],
+            aws_secret_access_key=self.config['secret_key'],
+        ).resource('s3')
+        self.bucket = self.s3.Bucket(self.config['bucket'])
+
+    @mock_s3
+    def test_from_config(self):
+        result = S3Exporter.from_config(self.config)
+        self.assertIsInstance(result, S3Exporter)
+
+    @mock_s3
+    def test_init(self):
+        S3Exporter(**self.config)
+        buckets = self.s3.buckets.all()
+        buckets = [x.name for x in buckets]
+        self.assertIn(self.config['bucket'], buckets)
+
+    @mock_s3
+    def test_export_asset(self):
+        exporter = S3Exporter(**self.config)
+        id_ = 'abc123'
+        expected = dict(asset_id=id_, foo='bar')
+        exporter._export_asset(expected)
+
+        with TemporaryDirectory() as root:
+            file_ = Path(root, f'{id}.json')
+            with open(file_, 'wb') as f:
+                self.bucket.download_fileobj(
+                    f'hidebound/metadata/asset/{id_}.json',
+                    f
+                )
+            with open(file_, 'r') as f:
+                self.assertEqual(json.load(f), expected)
+
+    @mock_s3
+    def test_export_file(self):
+        with TemporaryDirectory() as root:
+            n = 'p-proj001_spec001_d-desc_v001'
+            rel_path = f'projects/proj001/spec001/{n}/{n}_f0000.json'
+            filepath = Path(root, rel_path)
+
+            content = {'foo': 'bar'}
+            os.makedirs(filepath.parent, exist_ok=True)
+            with open(Path(root, filepath), 'w') as f:
+                json.dump(content, f)
+
+            exporter = S3Exporter(**self.config)
+            id_ = 'abc123'
+            expected = dict(
+                file_id=id_,
+                foo='bar',
+                filepath=filepath.as_posix(),
+                filepath_relative=rel_path,
+            )
+            exporter._export_file(expected)
+
+            # metadata
+            file_ = Path(root, 'metadata.json')
+            with open(file_, 'wb') as f:
+                self.bucket.download_fileobj(
+                    f'hidebound/metadata/file/{id_}.json',
+                    f
+                )
+            with open(file_, 'r') as f:
+                self.assertEqual(json.load(f), expected)
+
+            # content
+            file_ = Path(root, 'content.json')
+            with open(file_, 'wb') as f:
+                self.bucket.download_fileobj(f'hidebound/content/{rel_path}', f)
+            with open(file_, 'r') as f:
+                self.assertEqual(json.load(f), content)
