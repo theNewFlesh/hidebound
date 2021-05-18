947b8afec9aee8f164e9b07b7a13494a757722c7
Alex Braun <alexander.g.braun@gmail.com>
2021-05-18 01:48:37 +0000

    added s3_exporter to init

--- python/hidebound/exporters/__init__.py
+++ python/hidebound/exporters/__init__.py
@@ -1,3 +1,4 @@
 import hidebound.exporters.exporter_base  # noqa F401
 import hidebound.exporters.girder_exporter  # noqa F401
 import hidebound.exporters.mock_girder  # noqa F401
+import hidebound.exporters.s3_exporter  # noqa F401
