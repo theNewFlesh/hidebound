fd52250c3c52986095fd9d7ac81d98782538ee93
Alex Braun <alexander.g.braun@gmail.com>
2021-06-04 21:59:33 +0000

    added local disk exporter

---
+++ python/hidebound/exporters/local_disk_exporter.py
@@ -0,0 +1,101 @@
+from typing import Dict, Union
+
+from pathlib import Path
+import os
+import re
+import shutil
+
+from schematics import Model
+from schematics.types import StringType
+
+from hidebound.exporters.exporter_base import ExporterBase
+import hidebound.core.tools as hbt
+import hidebound.core.validators as vd
+# ------------------------------------------------------------------------------
+
+
+class LocalDiskConfig(Model):
+    '''
+    A class for validating configurations supplied to LocalDiskExporter.
+
+    Attributes:
+        target_directory (str): Target directory.
+    '''
+    target_directory = StringType(
+        required=True, validators=[vd.is_legal_directory]
+    )  # type: StringType
+
+
+class LocalDiskExporter(ExporterBase):
+    @staticmethod
+    def from_config(config):
+        # type: (Dict) -> LocalDiskExporter
+        '''
+        Construct a LocalDiskExporter from a given config.
+
+        Args:
+            config (dict): Config dictionary.
+
+        Raises:
+            DataError: If config is invalid.
+
+        Returns:
+            LocalDiskExporter: LocalDiskExporter instance.
+        '''
+        return LocalDiskExporter(**config)
+
+    def __init__(self, target_directory):
+        # type: (str) -> None
+        '''
+        Constructs a LocalDiskExporter instance.
+        Creates target directory if it does not exist.
+
+        Args:
+            target_directory (str): Target directory.
+
+        Raises:
+            DataError: If config is invalid.
+        '''
+        config = dict(target_directory=target_directory)
+        LocalDiskConfig(config).validate()
+        # ----------------------------------------------------------------------
+
+        self._target_directory = config['target_directory']
+        os.makedirs(self._target_directory, exist_ok=True)
+
+    def export(self, hidebound_dir):
+        # type: (Union[str, Path]) -> None
+        '''
+        Exports data within given hidebound directory.
+
+        Args:
+            hidebound_dir (Path or str): Hidebound directory.
+        '''
+        self._enforce_directory_structure(hidebound_dir)
+
+        hidebound_dir = Path(hidebound_dir).as_posix()
+        data = hbt.directory_to_dataframe(hidebound_dir)
+        data['target'] = data.filepath \
+            .apply(lambda x: re.sub(hidebound_dir, self._target_directory, x))
+        data.target.apply(lambda x: os.makedirs(Path(x).parent, exist_ok=True))
+        data.apply(lambda x: shutil.copy(x.filepath, x.target), axis=1)
+
+    def _export_asset(self, metadata):
+        # type: (Dict) -> None
+        '''
+        Exports metadata from single JSON file in hidebound/metadata/asset.
+
+        Args:
+            metadata (dict): Asset metadata.
+        '''
+        pass
+
+    def _export_file(self, metadata):
+        # type: (Dict) -> None
+        '''
+        Exports metadata from single JSON file in hidebound/metadata/file.
+
+        Args:
+            metadata (dict): File metadata.
+        '''
+        pass
---
+++ python/hidebound/exporters/local_disk_exporter_test.py
@@ -0,0 +1,132 @@
+from itertools import chain
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from uuid import uuid4
+import os
+import re
+import unittest
+
+from pandas import DataFrame
+from schematics.exceptions import DataError
+
+from hidebound.exporters.local_disk_exporter import LocalDiskConfig, LocalDiskExporter
+import hidebound.core.tools as hbt
+# ------------------------------------------------------------------------------
+
+
+class LocalDiskConfigTests(unittest.TestCase):
+    def test_validate(self):
+        config = dict(target_directory='/foo/bar')
+        LocalDiskConfig(config).validate()
+
+    def test_validate_errors(self):
+        expected = 'is not a legal directory path'
+        for path in ['foo/bar', '\foo\bar', '/foo/bar/', '/foo.bar/baz']:
+            with self.assertRaisesRegexp(DataError, expected):
+                config = dict(target_directory=path)
+                LocalDiskConfig(config).validate()
+# ------------------------------------------------------------------------------
+
+
+class LocalDiskExporterTests(unittest.TestCase):
+    def get_config(self, root):
+        return dict(target_directory=Path(root, 'target').as_posix())
+
+    def test_from_config(self):
+        with TemporaryDirectory() as root:
+            config = self.get_config(root)
+            result = LocalDiskExporter.from_config(config)
+            self.assertIsInstance(result, LocalDiskExporter)
+
+    def test_init(self):
+        with TemporaryDirectory() as root:
+            config = self.get_config(root)
+            result = LocalDiskExporter(**config)
+            self.assertIsInstance(result, LocalDiskExporter)
+            self.assertTrue(Path(config['target_directory']).is_dir())
+
+    def setup_hidebound_directory(self, root):
+        hb_root = Path(root, 'hidebound')
+        file_meta = Path(hb_root, 'metadata', 'file')
+        asset_meta = Path(hb_root, 'metadata', 'asset')
+        content = Path(hb_root, 'content')
+        data = []
+
+        # add sequence assets
+        proj = 'proj001'
+        spec = 'seq001'
+        for v in range(0, 2):
+            name = f'p-proj001_s-seq001_d-seq_v{v:03d}'
+            asset_id = str(uuid4())
+            for f in range(0, 3):
+                file_id = str(uuid4())
+                rel_path = Path(name, name + f'_f{f:04d}.png')
+                data.append(dict(
+                    project=proj,
+                    spec=spec,
+                    asset_name=name,
+                    asset_id=asset_id,
+                    file_id=file_id,
+                    a_path=Path(asset_meta, asset_id + '.json').as_posix(),
+                    f_path=Path(file_meta, file_id + '.json').as_posix(),
+                    c_path=Path(content, proj, spec, rel_path).as_posix()
+                ))
+
+        # add file asset
+        spec = 'text001'
+        filename = f'p-proj001_s-{spec}_d-file_v001.txt'
+        asset_id = str(uuid4())
+        data.append(dict(
+            project=proj,
+            spec=spec,
+            asset_name=name,
+            asset_id=asset_id,
+            file_id=None,
+            a_path=Path(asset_meta, asset_id + '.json').as_posix(),
+            f_path=None,
+            c_path=Path(content, proj, spec, filename).as_posix()
+        ))
+        data = DataFrame(data)
+
+        a_paths = data.a_path.dropna().unique().tolist()
+        f_paths = data.f_path.dropna().unique().tolist()
+        c_paths = data.c_path.dropna().unique().tolist()
+        filepaths = list(chain(a_paths, f_paths, c_paths))
+        for filepath in filepaths:
+            os.makedirs(Path(filepath).parent, exist_ok=True)
+            with open(filepath, 'w') as f:
+                f.write('data')
+
+        return hb_root
+
+    def test_export(self):
+        with TemporaryDirectory() as root:
+            config = self.get_config(root)
+            hb_root = self.setup_hidebound_directory(root)
+            exp = LocalDiskExporter.from_config(config)
+
+            target = config['target_directory']
+            self.assertEqual(len(os.listdir(target)), 0)
+
+            exp.export(hb_root)
+
+            expected = hbt.directory_to_dataframe(hb_root).filepath \
+                .apply(lambda x: re.sub('.*/hidebound/', '', x)) \
+                .tolist()
+            expected = sorted(expected)
+
+            result = hbt.directory_to_dataframe(target).filepath \
+                .apply(lambda x: re.sub('.*/target/', '', x)) \
+                .tolist()
+            result = sorted(result)
+
+            self.assertEqual(result, expected)
+
+            # idempotentcy
+            LocalDiskExporter.from_config(config).export(hb_root)
+            result = hbt.directory_to_dataframe(target).filepath \
+                .apply(lambda x: re.sub('.*/target/', '', x)) \
+                .tolist()
+            result = sorted(result)
+
+            self.assertEqual(result, expected)
