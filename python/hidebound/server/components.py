426f369812203d5b6bbe7cae8ffee28080591058
Alex Braun <alexander.g.braun@gmail.com>
2022-07-06 23:16:52 +0000

    content-container

--- python/hidebound/server/components.py
+++ python/hidebound/server/components.py
@@ -130,7 +130,11 @@ def get_dash_app(server, storage_type='memory'):
             )
         ],
     )
-    content = html.Div(id="content", className='content')
+    content = html.Div(
+        id="content-container",
+        className='content-container',
+        children=[html.Div(id="content", className='content')],
+    )
 
     app = dash.Dash(
         name='hidebound',
