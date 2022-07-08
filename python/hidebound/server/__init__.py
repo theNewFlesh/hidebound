1ad42c6b5a345295ac55770017a98f200c777bc8
Alex Braun <alexander.g.braun@gmail.com>
2022-07-08 06:01:20 +0000

    removed progress from __init__

--- python/hidebound/server/__init__.py
+++ python/hidebound/server/__init__.py
@@ -4,5 +4,4 @@ import hidebound.server.api  # noqa F401
 import hidebound.server.extension  # noqa F401
 import hidebound.server.extensions  # noqa F401
 import hidebound.server.components  # noqa F401
-import hidebound.server.progress  # noqa F401
 import hidebound.server.server_tools  # noqa F401
