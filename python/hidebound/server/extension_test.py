27ee667add6edfda4cf161ccbcee576ca1679438
Alex Braun <alexander.g.braun@gmail.com>
2022-09-19 21:04:52 +0000

    fixed broken tests

--- python/hidebound/server/extension_test.py
+++ python/hidebound/server/extension_test.py
@@ -8,48 +8,48 @@ from hidebound.server.extension import HideboundExtension
 # ------------------------------------------------------------------------------
 
 
-def test_init(env, app, config, make_dirs):
+def test_init(env, flask_app, config, make_dirs):
     result = HideboundExtension(app=None)
     assert hasattr(result, 'config') is False
     assert hasattr(result, 'database') is False
 
-    app.config['TESTING'] = False
-    result = HideboundExtension(app=app)
+    flask_app.config['TESTING'] = False
+    result = HideboundExtension(app=flask_app)
     assert hasattr(result, 'config')
     assert hasattr(result, 'database')
     assert result.config == config
     assert isinstance(result.database, Database)
 
 
-def test_get_config_from_env(env, app, config):
-    app.config.from_prefixed_env('HIDEBOUND')
-    result = HideboundExtension()._get_config_from_env(app)
+def test_get_config_from_env(env, flask_app, config):
+    flask_app.config.from_prefixed_env('HIDEBOUND')
+    result = HideboundExtension()._get_config_from_env(flask_app)
     for key, val in config.items():
         assert result[key] == val
 
 
-def test_get_config_from_env_bool(env, app, config):
+def test_get_config_from_env_bool(env, flask_app, config):
     config['dask_enabled'] = True
     os.environ['HIDEBOUND_DASK_ENABLED'] = 'True'
-    app.config.from_prefixed_env('HIDEBOUND')
-    result = HideboundExtension()._get_config_from_env(app)
+    flask_app.config.from_prefixed_env('HIDEBOUND')
+    result = HideboundExtension()._get_config_from_env(flask_app)
     for key, val in config.items():
         assert result[key] == val
 
 
-def test_get_config_from_yaml_file(env, app, config, config_yaml_file):
+def test_get_config_from_yaml_file(env, flask_app, config, config_yaml_file):
     result = HideboundExtension()._get_config_from_file(config_yaml_file)
     for key, val in config.items():
         assert result[key] == val
 
 
-def test_get_config_from_json_file(env, app, config, config_json_file):
+def test_get_config_from_json_file(env, flask_app, config, config_json_file):
     result = HideboundExtension()._get_config_from_file(config_json_file)
     for key, val in config.items():
         assert result[key] == val
 
 
-def test_get_config_from_file_error(env, app, config):
+def test_get_config_from_file_error(env, flask_app, config):
     expected = 'Hidebound config files must end in one of these extensions:'
     expected += r" \['json', 'yml', 'yaml'\]\. Given file: "
     expected += r'/foo/bar/config\.pizza\.'
@@ -57,37 +57,37 @@ def test_get_config_from_file_error(env, app, config):
         HideboundExtension()._get_config_from_file('/foo/bar/config.pizza')
 
 
-def test_get_config_env_vars(env, app, config):
-    app.config.from_prefixed_env('HIDEBOUND')
-    result = HideboundExtension()._get_config(app)
+def test_get_config_env_vars(env, flask_app, config):
+    flask_app.config.from_prefixed_env('HIDEBOUND')
+    result = HideboundExtension()._get_config(flask_app)
     for key, val in config.items():
         assert result[key] == val
 
 
-def test_get_config_mising_env_var(env, app, config):
+def test_get_config_mising_env_var(env, flask_app, config):
     os.environ.pop('HIDEBOUND_WORKFLOW')
-    app.config.from_prefixed_env('HIDEBOUND')
-    result = HideboundExtension()._get_config(app)['workflow']
+    flask_app.config.from_prefixed_env('HIDEBOUND')
+    result = HideboundExtension()._get_config(flask_app)['workflow']
     expected = ['delete', 'update', 'create', 'export']
     assert result == expected
 
 
-def test_get_config_env_vars_empty_lists(env, app, config):
+def test_get_config_env_vars_empty_lists(env, flask_app, config):
     os.environ['HIDEBOUND_WORKFLOW'] = '[]'
     os.environ['HIDEBOUND_SPECIFICATION_FILES'] = '[]'
     os.environ['HIDEBOUND_WEBHOOKS'] = '[]'
-    app.config.from_prefixed_env('HIDEBOUND')
+    flask_app.config.from_prefixed_env('HIDEBOUND')
 
     keys = ['workflow', 'specification_files', 'webhooks']
     for key in keys:
-        result = HideboundExtension()._get_config(app)[key]
+        result = HideboundExtension()._get_config(flask_app)[key]
         assert result == []
 
 
-def test_get_config_filepath(env, app, config, config_yaml_file):
+def test_get_config_filepath(env, flask_app, config, config_yaml_file):
     os.environ['HIDEBOUND_INGRESS_DIRECTORY'] = 'foobar'
-    app.config.from_prefixed_env('HIDEBOUND')
-    result = HideboundExtension()._get_config(app)
+    flask_app.config.from_prefixed_env('HIDEBOUND')
+    result = HideboundExtension()._get_config(flask_app)
 
     assert result['ingress_directory'] != 'foobar'
 
@@ -95,25 +95,25 @@ def test_get_config_filepath(env, app, config, config_yaml_file):
         assert result[key] == val
 
 
-def test_init_app(env, app, config, make_dirs):
-    app.config['TESTING'] = False
+def test_init_app(env, flask_app, config, make_dirs):
+    flask_app.config['TESTING'] = False
     hb = HideboundExtension()
-    hb.init_app(app)
+    hb.init_app(flask_app)
 
-    assert app.extensions['hidebound'] is hb
-    assert app.blueprints['hidebound_api'] is API
+    assert flask_app.extensions['hidebound'] is hb
+    assert flask_app.blueprints['hidebound_api'] is API
     assert hasattr(hb, 'config')
     assert hasattr(hb, 'database')
     assert hb.config == config
     assert isinstance(hb.database, Database)
 
 
-def test_init_app_testing(env, app, config):
-    app.config['TESTING'] = True
+def test_init_app_testing(env, flask_app, config):
+    flask_app.config['TESTING'] = True
     hb = HideboundExtension()
-    hb.init_app(app)
+    hb.init_app(flask_app)
 
-    assert app.extensions['hidebound'] is hb
-    assert app.blueprints['hidebound_api'] is API
+    assert flask_app.extensions['hidebound'] is hb
+    assert flask_app.blueprints['hidebound_api'] is API
     assert hasattr(hb, 'config') is False
     assert hasattr(hb, 'database') is False
