aedc2de50c6b7178f267aa6aad780eb589798962
Alex Braun <alexander.g.braun@gmail.com>
2022-12-06 01:59:38 +0000

    added command.py

--- python/hidebound/__init__.py
+++ python/hidebound/__init__.py
@@ -5,6 +5,7 @@ Author: Alex Braun
 Email: Alexander.G.Braun@gmail.com
 Github: https://github.com/theNewFlesh
 '''
+import hidebound.command  # noqa F401
 import hidebound.core  # noqa F401
 import hidebound.exporters  # noqa F401
 import hidebound.server  # noqa F401
